d0c32c9b6eb3656eb5fbe3a846eb1eb6d344f0de
Eric MacDonald <ericmacdonald@Erics-MacBook-Pro.local>
2018-01-17 16:57:21 -0500

    remoived some repeditive code with loops and variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 //establish a timer for the game
 var timeOut=10;
 var counter;
+var quizLength = 4;
+var answerLength = 4;
+
 
 
 // create all of the questions and answers as objects
@@ -41,121 +44,36 @@ var question4 = {
 	correctAnswer: "answer1"
 };
 
-var results = {
-	question1: false,
-	question2: false,
-	question3: false,
-	question4: false,
-};
 
-//hide all of the questions until you press begin
-$("#gameblock").hide();
-//hide the results
-$(".result_block").hide();
-
-//push the content onto the page when you ckick "begin"
-
-$(".btn-begin").on("click", function() {
-	$(".btn-begin").hide();
-	$("#gameblock").show();
-	counter=setInterval(timer, 1000);
-
-	$("#question1").text(question1.question);
-	$("#1-1").text(question1.answer1);
-	$("#1-2").text(question1.answer2);
-	$("#1-3").text(question1.answer3);
-	$("#1-4").text(question1.answer4);
-
-	$("#question2").text(question2.question);
-	$("#2-1").text(question2.answer1);
-	$("#2-2").text(question2.answer2);
-	$("#2-3").text(question2.answer3);
-	$("#2-4").text(question2.answer4);
-
-	$("#question3").text(question3.question);
-	$("#3-1").text(question3.answer1);
-	$("#3-2").text(question3.answer2);
-	$("#3-3").text(question3.answer3);
-	$("#3-4").text(question3.answer4);
-
-	$("#question4").text(question4.question);
-	$("#4-1").text(question4.answer1);
-	$("#4-2").text(question4.answer2);
-	$("#4-3").text(question4.answer3);
-	$("#4-4").text(question4.answer4);
-});
-
-
-
-var guessArray=["answers"];
 var correctCount=0;
 var incorrectCount=0;
 var unguessed=0;
-var guess1;
-var quess2;
-var guess3;
-var quess4;
 
 
 function checkResults() {
 	clearInterval(counter);
-	guess1 = $("input[name=answer1]:checked").val();
-	guess2 = $("input[name=answer2]:checked").val();
-	guess3 = $("input[name=answer3]:checked").val();
-	guess4 = $("input[name=answer4]:checked").val();
 
-	console.log(guess1 + "  " + guess2 + "  " + guess3 + "  " + guess4);
-	console.log(question1.correctAnswer + "  " + question2.correctAnswer + "  " + question3.correctAnswer + "  " + question4.correctAnswer);
+	var guess1 = $("input[name=answer1]:checked").val();
+	var guess2 = $("input[name=answer2]:checked").val();
+	var guess3 = $("input[name=answer3]:checked").val();
+	var guess4 = $("input[name=answer4]:checked").val();
 
-
-	if (guess1 === question1.correctAnswer) {
-		correctCount++;
+	for (i=1; i<quizLength+1; i++) {
+		// var guessIndex = eval ($("input[name=answer" + i + "]:checked").val());
+		var questionIndex = eval ("question" + i);
+		if (eval("guess" + i) === questionIndex["correctAnswer"]) {
+			correctCount++;
 		}
-		else if (guess1 === undefined) {
-			$(".gameblock1").removeClass("alert-success").addClass("bg-danger");
+		else if (eval("guess" + i) === undefined) {
+			$(".gameblock" + i).removeClass("alert-success").addClass("bg-danger");
 			unguessed++;
 		}
 		else {
-			$(".gameblock1").removeClass("alert-success").addClass("bg-danger");
+			$(".gameblock" + i).removeClass("alert-success").addClass("bg-danger");
 			incorrectCount++;	
 		}
-	if (guess2 === question2.correctAnswer) {
-		correctCount++;
-		}
-		else if (guess2 === undefined) {
-			$(".gameblock2").removeClass("alert-success").addClass("bg-danger");
-			unguessed++;
-		}
-		else {
-		$(".gameblock2").removeClass("alert-success").addClass("bg-danger");
-		incorrectCount++;
-		}
-	if (guess3 === question3.correctAnswer) {
-		correctCount++;
-		}
-		else if (guess3 === undefined) {
-			$(".gameblock3").removeClass("alert-success").addClass("bg-danger");
-			unguessed++;
-		}
-		else {
-		$(".gameblock3").removeClass("alert-success").addClass("bg-danger");
-		incorrectCount++;
-		}
-	if (guess4 === question4.correctAnswer) {
-		correctCount++;
-		}
-		else if (guess4 === undefined) {
-			$(".gameblock4").removeClass("alert-success").addClass("bg-danger");
-			unguessed++;
-		}
-		else {
-		$(".gameblock4").removeClass("alert-success").addClass("bg-danger");
-		incorrectCount++;
-		}		
-
-	console.log("correct answers: " + correctCount);
-	console.log("incorrect answers: " + incorrectCount);
-	return;
+	}
+		return;
 }
 
 function pushResults(){
@@ -167,7 +85,7 @@ function pushResults(){
 	$("#score").text("Score: " + (correctCount / 4)*100 + "%");
 }
 
-
+//count down
 function timer() {
 	if (timeOut<0) {
 		$(".btn").removeClass("btn-success").addClass("btn-danger").text("Out of time, click for your score");
@@ -179,7 +97,39 @@ function timer() {
 	timeOut--
 }
 
+//hide all of the questions until you press begin
+$("#gameblock").hide();
+//hide the results
+$(".result_block").hide();
+//hide done button
+$(".btn-done").hide();
+$("#time_remaining").hide();
+
+
+//build page when you click "begin"
+$(".btn-begin").on("click", function() {
+	$(".btn-begin").hide();
+	$(".btn-done").show();
+	$("#gameblock").show();
+	$("#time_remaining").show();
+	counter=setInterval(timer, 1000);
+
+	var q;
+	var a;
+	
+//push questions onto page
+	for (q=1; q<quizLength+1; q++) {
+		var questionIndex = eval("question" + q);
+
+		$("#question"+ q).text(questionIndex.question);
+		for (a=1; a<answerLength+1; a++)
+			$("#" + q + "-" + a).text(questionIndex["answer" + a]);
+	}
+
+//when the user clicks "done", check results and push results
 $(".btn-done").on("click", function() {
 	checkResults();
 	pushResults();
+});
+
 });
